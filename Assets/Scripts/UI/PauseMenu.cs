432d95604be3305748f2e3aea47b92c15c58d0dd
Michael Wolfe <mjw323@drexel.edu>
2013-02-15 17:05:48 -0500

    Bug Fix
    Temporarily removed Options that make game unfair, removed restart,
    changed functionality of "Quit".

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class PickMenu : MonoBehaviour {
+public class PauseMenu : MonoBehaviour {
 	public static bool showMenu;
 	public bool pause;
 	public GUIStyle pm;
@@ -31,12 +31,14 @@ public class PickMenu : MonoBehaviour {
 				pause = false;
 				Time.timeScale = 1.0f;
 			}
-			else if (GUI.Button(new Rect(Screen.width/2-Screen.width/10,Screen.height/2,Screen.width/5,Screen.height/10),"Restart",pm)) {
-				pause = false;
-				Time.timeScale = 1.0f;
-				Application.LoadLevel(1);
-			}
+			//else if (GUI.Button(new Rect(Screen.width/2-Screen.width/10,Screen.height/2,Screen.width/5,Screen.height/10),"Restart",pm)) {
+			//	pause = false;
+			//	Time.timeScale = 1.0f;
+			//	Application.LoadLevel(1);
+			//}
 			else if (GUI.Button(new Rect(Screen.width/2-Screen.width/10,Screen.height/2+Screen.height/8,Screen.width/5,Screen.height/10),"Quit",pm)) {
+				Network.Disconnect();
+				MasterServer.UnregisterHost();
 				pause = false;
 				Time.timeScale = 1.0f;
 				Application.LoadLevel(0);
