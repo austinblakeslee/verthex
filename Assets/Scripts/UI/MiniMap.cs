36cb9117ce518a6683dfb25395b77fe7114cf6b4
Austin <atb046@gmail.comcd>
2013-02-20 17:38:49 -0500

    MiniMap

--- Assets/Scripts/UI/MiniMap.cs
+++ Assets/Scripts/UI/MiniMap.cs
@@ -16,12 +16,12 @@ public class MiniMap : MonoBehaviour {
 	void Start() {
 		ow = 960;
 		oh = 600;
-		p1T1Rect = new Rect(5,435,100,50);//Screen.width/2,Screen.height-100,80,20);
-		p1T2Rect = new Rect(5,490,100,50);//Screen.width/2,Screen.height-66,80,20);
-		p1T3Rect = new Rect(5,545,100,50);//Screen.width/2,Screen.height-33,80,20);
-		p2T1Rect = new Rect(105,435,100,50);//Screen.width/2+100,Screen.height-100,80,20);
-		p2T2Rect = new Rect(105,490,100,50);//Screen.width/2+100,Screen.height-66,80,20);
-		p2T3Rect = new Rect(105,545,100,50);//Screen.width/2+100,Screen.height-33,80,20);
+		p1T1Rect = new Rect(147,430,30,30);//new Rect(80,435,20,20);
+		p1T2Rect = new Rect(75,430,30,30);//new Rect(20,480,20,20);
+		p1T3Rect = new Rect(15,475,30,30);//new Rect(50,565,20,20);
+		p2T1Rect = new Rect(45,560,30,30);
+		p2T2Rect = new Rect(117,560,30,30);//new Rect(183,520,20,20);
+		p2T3Rect = new Rect(178,515,30,30);
 		MenuItemManager.RegisterRect(p1T1Rect);
 		MenuItemManager.RegisterRect(p1T2Rect);
 		MenuItemManager.RegisterRect(p1T3Rect);
@@ -37,37 +37,37 @@ public class MiniMap : MonoBehaviour {
 		float scaleX = Screen.width/ow;
 		Matrix4x4 svMat = GUI.matrix;
 		GUI.matrix = Matrix4x4.TRS(new Vector3((scaleX - scale.y)/2 * ow,0,0),Quaternion.identity,scale);
-		if(GUI.Button(p1T1Rect, "P1:T1"))
+		if(GUI.Button(p1T1Rect, "1:1"))
 		{
 			Tower baseTow = TurnOrder.GetPlayerByNumber(1).GetTower(0);
 			TowerSelection.LocalSelectSection(baseTow, -1);
 		}
 
-		if(GUI.Button(p1T2Rect, "P1:T2"))
+		if(GUI.Button(p1T2Rect, "1:2"))
 		{
 			Tower baseTow = TurnOrder.GetPlayerByNumber(1).GetTower(1);
 			TowerSelection.LocalSelectSection(baseTow, -1);
 		}
 		
-		if(GUI.Button(p1T3Rect, "P1:T3"))
+		if(GUI.Button(p1T3Rect, "1:3"))
 		{
 			Tower baseTow = TurnOrder.GetPlayerByNumber(1).GetTower(2);
 			TowerSelection.LocalSelectSection(baseTow, -1);
 		}
 		
-		if(GUI.Button(p2T1Rect, "P2:T1"))
+		if(GUI.Button(p2T1Rect, "2:1"))
 		{
 			Tower baseTow = TurnOrder.GetPlayerByNumber(2).GetTower(0);
 			TowerSelection.LocalSelectSection(baseTow, -1);
 		}
 		
-		if(GUI.Button(p2T2Rect, "P2:T2"))
+		if(GUI.Button(p2T2Rect, "2:2"))
 		{
 			Tower baseTow = TurnOrder.GetPlayerByNumber(2).GetTower(1);
 			TowerSelection.LocalSelectSection(baseTow, -1);
 		}
 		
-		if(GUI.Button(p2T3Rect, "P2:T3"))
+		if(GUI.Button(p2T3Rect, "2:3"))
 		{
 			Tower baseTow = TurnOrder.GetPlayerByNumber(2).GetTower(2);
 			TowerSelection.LocalSelectSection(baseTow, -1);
