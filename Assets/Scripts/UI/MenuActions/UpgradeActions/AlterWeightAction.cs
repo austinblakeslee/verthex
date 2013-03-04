a02c0c8f64b2f2abcd0fee278464f36a63f406c5
Priyal Seebadri <prs43@drexel.edu>
2013-03-03 23:40:35 -0500

    Began to update the Upgrade menu with the final weapon upgrades

---
+++ Assets/Scripts/UI/MenuActions/UpgradeActions/AlterWeightAction.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class ForceFieldAction : DefaultMenuAction,MenuAction {
+	
+	public override void Action() {
+		Section s = TowerSelection.GetSelectedSection();
+		if(s != null) {
+			int cost = 200;
+			Player p = TurnOrder.myPlayer;
+			WeaponEffect effect = s.attributes.weapon.GetEffect();
+			if(effect.GetEffectType() == "Force Field") {
+				ValueStore.helpMessage = "Cannot add Force Field effect. Force Field effect already purchased!";
+			} else if(effect.GetUpgradeLevel() >= 3) {
+				ValueStore.helpMessage = "The weapon effect is already at max!";
+			} else if(cost > p.GetResources()) {
+				ValueStore.helpMessage = "You don't have enough RP to do that!";
+			} else {
+				TurnOrder.SendAction(new Upgrade(s.attributes.myTower.towerNum, s.attributes.height, "Force Field"));
+			}
+		} else {
+			ValueStore.helpMessage = "You must select a section to upgrade!";
+		}
+	}
+}
---
+++ Assets/Scripts/UI/MenuActions/UpgradeActions/ForceFieldAction.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class AoEAction : DefaultMenuAction,MenuAction {
+	
+	public override void Action() {
+		Section s = TowerSelection.GetSelectedSection();
+		if(s != null) {
+			int cost = 200;
+			Player p = TurnOrder.myPlayer;
+			WeaponEffect effect = s.attributes.weapon.GetEffect();
+			if(effect.GetEffectType() == "Burn") {
+				ValueStore.helpMessage = "Cannot add Multi effect. Burn effect already purchased!";
+			} else if(effect.GetUpgradeLevel() >= 3) {
+				ValueStore.helpMessage = "The weapon effect is already at max!";
+			} else if(cost > p.GetResources()) {
+				ValueStore.helpMessage = "You don't have enough RP to do that!";
+			} else {
+				TurnOrder.SendAction(new Upgrade(s.attributes.myTower.towerNum, s.attributes.height, "AoE"));
+			}
+		} else {
+			ValueStore.helpMessage = "You must select a section to upgrade!";
+		}
+	}
+	
+}
