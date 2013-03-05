20d85f00b7bd5b283a45f120d8daaa99ab1e2b4a
Zachary Thacker <zpthacker@gmail.com>
2013-03-05 02:41:12 -0500

    Update Title scene so it's a bit more scenic

--- Assets/Scripts/Environment/Levitation.cs
+++ Assets/Scripts/Environment/Levitation.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class Levitation : MonoBehaviour {
 
+	float oscSpeed;
+	float amplitude;
+
 	// Use this for initialization
 	void Start () {
-	
+		oscSpeed = Random.Range(1.0f, 5.0f);
+		amplitude = Random.Range(1.0f, 3.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate(0, Mathf.Sin(Time.time) * Time.deltaTime, 0);
+		transform.Translate(0, Mathf.Sin(oscSpeed * Time.time) * Time.deltaTime * amplitude, 0);
 	}
 }
