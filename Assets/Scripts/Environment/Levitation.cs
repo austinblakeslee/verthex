0f2235847091ef864874d365325e467fc9ae8b1a
Zachary Thacker <zpthacker@gmail.com>
2013-03-05 13:35:07 -0500

    Add small oscillation to aliens

--- Assets/Scripts/Environment/Levitation.cs
+++ Assets/Scripts/Environment/Levitation.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class Levitation : MonoBehaviour {
 
-	float oscSpeed;
-	float amplitude;
+	public float oscSpeed = 5.0f;
+	public float amplitude = 3.0f;
 
 	// Use this for initialization
 	void Start () {
-		oscSpeed = Random.Range(1.0f, 5.0f);
-		amplitude = Random.Range(1.0f, 3.0f);
+		oscSpeed = Random.Range(1.0f, oscSpeed);
+		amplitude = Random.Range(1.0f, amplitude);
 	}
 	
 	// Update is called once per frame
