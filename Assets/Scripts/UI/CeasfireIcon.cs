bb7d45a3453b226e8b98f4d6babba91fe3c9f0f8
Austin <atb046@gmail.comcd>
2013-03-13 02:29:03 -0400

    Ceasefire Banners

---
+++ Assets/Scripts/UI/CeasfireIcon.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+
+public class CeasfireIcon : MonoBehaviour {
+	
+	public Texture2D cfIcon;
+	public Texture2D nocfIcon;
+	private Vector3 scale;
+	private float ow;
+	private float oh;
+	
+	// Use this for initialization
+	void Start () {
+		ow = 960;
+		oh = 600;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnGUI() {
+		scale.y = Screen.height/oh;
+		scale.x = Screen.width/ow;
+		scale.z = 1;
+		float scaleX = Screen.width/ow;
+		GUI.matrix = Matrix4x4.TRS(new Vector3((scaleX - scale.y)/2 * ow,0,0),Quaternion.identity,scale);
+		if(TurnOrder.ceasefire > TurnOrder.turnNum) {
+			GUI.Label (new Rect(Screen.width/2-150,0,300,300),cfIcon);
+		}
+		else if (TurnOrder.ceasefire == TurnOrder.turnNum)  {
+			GUI.Label (new Rect(Screen.width/2-150,0,300,300),nocfIcon);
+		}
+	}
+}
